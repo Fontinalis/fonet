b3a164f342eee41d314ef6219628536268a413ec
Barnabás Pataki <barnabas.vac@gmail.com>
2017-10-03 19:16:49 +0200

    Add XOR example

---
+++ examples/xor/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/Fontinalis/fonet"
+)
+
+var samples [][][]float64 = [][][]float64{
+	[][]float64{
+		[]float64{
+			0,
+			0,
+		},
+		[]float64{
+			0,
+		},
+	},
+	[][]float64{
+		[]float64{
+			0,
+			1,
+		},
+		[]float64{
+			1,
+		},
+	},
+	[][]float64{
+		[]float64{
+			1,
+			0,
+		},
+		[]float64{
+			1,
+		},
+	},
+	[][]float64{
+		[]float64{
+			1,
+			1,
+		},
+		[]float64{
+			0,
+		},
+	},
+}
+
+func main() {
+	n, err := fonet.NewNetwork([]int{2, 3, 1})
+	if err != nil {
+		log.Fatal(err)
+	}
+	n.Train(samples, 100000, 1.001)
+	fmt.Println(n.Predict([]float64{0, 0}))
+	fmt.Println(n.Predict([]float64{0, 1}))
+	fmt.Println(n.Predict([]float64{1, 0}))
+	fmt.Println(n.Predict([]float64{1, 1}))
+}
