fffba579c979bf5fa1acc56ae5b8faecc05c7c41
Ben Cragg <bcvery1@gmail.com>
2019-08-02 16:20:36 +0100

    Cleaned up test struct

--- network_test.go
+++ network_test.go
@@ -87,49 +87,32 @@ func TestNetwork(t *testing.T) {
 	tests := []struct {
 		name    string
 		samples [][][]float64
-		inps    [][]float64
-		res     []float64
+		results [][][]float64
 	}{
 		{
 			name:    "Output range 0-1",
 			samples: samples,
-			inps: [][]float64{
-				{0, 0},
-				{0, 1},
-				{1, 0},
-				{1, 1},
-			},
-			res: []float64{
-				0,
-				1,
-				1,
-				0,
+			results: [][][]float64{
+				{{0, 0}, {0}},
+				{{0, 1}, {1}},
+				{{1, 0}, {1}},
+				{{1, 1}, {0}},
 			},
 		},
 		{
 			name:    "Output range 10-20",
 			samples: samples,
-			inps: [][]float64{
-				{0, 0},
-				{0, 1},
-				{1, 0},
-				{1, 1},
-			},
-			res: []float64{
-				10,
-				20,
-				20,
-				10,
+			results: [][][]float64{
+				{{0, 0}, {10}},
+				{{0, 1}, {20}},
+				{{1, 0}, {20}},
+				{{1, 1}, {10}},
 			},
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t1 *testing.T) {
-			if len(tt.inps) != len(tt.res) {
-				t1.Fatalf("Different amount of test data [%d] vs results [%d]", len(tt.inps), len(tt.res))
-			}
-
 			n, err := NewNetwork([]int{2, 3, 1})
 			if err != nil {
 				t1.Fatalf("Could not create network: %+v", err)
@@ -137,9 +120,9 @@ func TestNetwork(t *testing.T) {
 
 			n.Train(tt.samples, 10000, 1.01, false)
 
-			for i, testData := range tt.inps {
-				if res := n.Predict(testData)[0]; !percDiffLessThan(res, tt.res[i], 2) {
-					t1.Errorf("Result is too different to be accurate; Got: %.2f, expected: %.2f", res, tt.res[i])
+			for _, exp := range tt.results {
+				if res := n.Predict(exp[0])[0]; !percDiffLessThan(res, exp[1][0], 2) {
+					t1.Errorf("Result is too different to be accurate; Got: %.2f, expected: %.2f", res, exp[1][0])
 				}
 			}
 		})
